f7a31e65ff23281f6d124c09874271b7e3f06172
Tawkir711 <mdtawkirk711@gmail.com>
2023-11-08 13:11:35 +0600

    added delete method

--- index.js
+++ index.js
@@ -85,6 +85,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/wishlist2', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: id }
+      const result = await postCollection.deleteOne(query)
+      res.send(result);
+    })
+
     app.put('/blogsId/:id', async (req, res) => {
       const id = req.params.id;
       const update = req.body;
