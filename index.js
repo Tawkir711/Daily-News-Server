5cd27e0bf1c046ae2734999b052bd335027d6fad
Tawkir711 <mdtawkirk711@gmail.com>
2023-11-09 19:50:17 +0600

    added jwt

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173' || 'https://daily-news-fd9d3.web.app'
+  ],
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 
@@ -22,6 +30,24 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  // console.log('token in the middleware', token);
+
+  // no token available
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+    req.user = decoded;
+    next();
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -29,6 +55,28 @@ async function run() {
 
     const blogCollection = client.db('blogPage').collection('dailyNews')
     const postCollection = client.db('blogPage').collection('postBlog')
+
+
+
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      console.log('user tokeN', user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+      })
+        .send({ success: true });
+    })
+
+    app.post('/logout', async (req, res) => {
+      const user = req.body;
+      console.log('logout', user);
+      res.clearCookie('token', { maxAge: 0 }).send({ success: true });
+    })
+
+
 
     app.get('/allBlog', async (req, res) => {
       const cursor = blogCollection.find();
@@ -70,8 +118,12 @@ async function run() {
     })
 
     
-    app.get('/wishlist2', async (req, res) => {
+    app.get('/wishlist2', verifyToken, async (req, res) => {
       console.log(req.query.email);
+      console.log('cok cok ', req.user);
+      if (req.user.email !== req.query.email) {
+        return res.status(403).send({message: 'forbidden access'})
+      }
       let query = {};
       if (req.query?.email) {
         query= {email: req.query.email}
